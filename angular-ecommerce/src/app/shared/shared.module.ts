923e232579650dae1cbbcc4e5a5d010d7e399547
Mahmudul Hasan <rakibsharif1999@gmail.com>
2024-08-31 16:14:41 +0600

    icon library setup

--- angular-ecommerce/src/app/shared/shared.module.ts
+++ angular-ecommerce/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../shared/header/header.component'
-
+import { HeaderComponent } from '../shared/header/header.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { heroUsers } from '@ng-icons/heroicons/outline'; 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgIconsModule.withIcons({ heroUsers })
   ],
   exports:[HeaderComponent]
 })
