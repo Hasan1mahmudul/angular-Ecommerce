ccef43ee53d8ba169fb6f0fcc6748fd6d6bdf654
Mahmudul Hasan <rakibsharif1999@gmail.com>
2024-09-02 19:27:25 +0600

    implement some design on header component

--- angular-ecommerce/src/app/shared/shared.module.ts
+++ angular-ecommerce/src/app/shared/shared.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { NgIconsModule } from '@ng-icons/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/header/header.component';
-import { heroUsers, heroPhone } from '@ng-icons/heroicons/outline';
+import { heroUsers, heroPhone, heroMagnifyingGlass, heroUser, heroHeart, heroShoppingCart, heroBars3 } from '@ng-icons/heroicons/outline';
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
-    NgIconsModule.withIcons({ heroUsers, heroPhone }),
+    NgIconsModule.withIcons({ heroUsers, heroPhone, heroMagnifyingGlass, heroUser, heroHeart, heroShoppingCart, heroBars3 }),
   ],
   exports:[HeaderComponent]
 })
